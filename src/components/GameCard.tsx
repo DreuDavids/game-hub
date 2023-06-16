9b26f2200e55a5eed6751b9f9aff45350f4612fd
DreuDavids <dreu.davids1@gmail.com>
2023-06-16 14:43:06 +0200

    Refactor: removed duplicated styles (GameCard and Skeleton Game Card) to GameCardContainer

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -12,7 +12,7 @@ interface Props {
  */
 const GameCard = ({ game }: Props) => {
   return (
-    <Card width="250px" borderRadius={15} overflow="hidden">
+    <Card>
       <Image src={getCroppedImageUrl(game.background_image)} />
       <CardBody>
         <Heading fontSize={"2xl"}>{game.name}</Heading>
--- src/components/GameCardSkeleton.tsx
+++ src/components/GameCardSkeleton.tsx
@@ -2,7 +2,7 @@ import { Card, CardBody, Skeleton, SkeletonText } from "@chakra-ui/react";
 
 const GameCardSkeleton = () => {
   return (
-    <Card width="250px" borderRadius={15} overflow="hidden">
+    <Card>
       <Skeleton height="200px" />
       <CardBody>
         <SkeletonText />
